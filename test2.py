33bad2e2b1cf68e44cfa84fb7a88807f1119f961
Azazel0203 <aadarshkr.singh.cd.ece21@itbhu.ac.in>
2024-03-30 04:11:02 +0530

    update

--- test2.py
+++
@@ -1,9 +0,0 @@
-# # Load model directly
-# from transformers import TFAutoModel
-# try:
-#     model = TFAutoModel.from_pretrained("Aad456334/ocr_captcha")
-#     print ("done")
-# except Exception as e:
-#     print(e)
-
-
