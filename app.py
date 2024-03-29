f1f29a5bcab334c54167a258cc4e3d600324e222
Azazel0203 <aadarshkr.singh.cd.ece21@itbhu.ac.in>
2024-03-30 05:03:23 +0530

    update

--- app.py
+++ app.py
@@ -54,9 +54,6 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
