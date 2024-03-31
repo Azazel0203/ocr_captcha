5ccd670257e9316672f153c5875e831ba43a7213
Azazel0203 <aadarshkr.singh.cd.ece21@itbhu.ac.in>
2024-03-31 19:01:35 +0530

    test_update

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
 
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port=8000)
